d271e8e21cddd3e1b10fc7fb8508fc65f7b12daf
Adrien <adrien.fournier1998@hotmail.fr>
2025-01-23 20:22:53 +0100

    add vercel analytics + update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -15,6 +16,36 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Mon vélib',
   description: 'Trouvez une station vélib près de vous',
+  generator: "Next.js",
+  keywords: ["velib", "station", "paris", "velo", "bicycle", "map", "react", "nextjs", "tailwindcss"],
+  openGraph: {
+    title: "Mon vélib",
+    description:
+      "Trouvez une station vélib près de vous.",
+    url: "https://velib-moi-ca.vercel.app/",
+    siteName: "velib-moi-ca.vercel.app",
+    locale: "fr-FR",
+    type: "website",
+  },
+  twitter: {
+    title: "Mon vélib",
+    description:
+      "Trouvez une station vélib près de vous.",
+    creator: "@adfrn",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -28,6 +59,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
